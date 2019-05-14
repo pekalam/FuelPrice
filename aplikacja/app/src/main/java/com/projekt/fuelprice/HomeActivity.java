abc266fc119ef6fafc1a0bd0b28dbd513ad2e2e2
Marek <marekbf3@gmail.com>
2019-05-14 11:40:43 +0200

    Fabric

--- aplikacja/app/src/main/java/com/projekt/fuelprice/HomeActivity.java
+++ aplikacja/app/src/main/java/com/projekt/fuelprice/HomeActivity.java
@@ -2,12 +2,15 @@ package com.projekt.fuelprice;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class HomeActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_home);
     }
 }
