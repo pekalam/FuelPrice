58b9bba2342bd7040b4ce0ebf581903c1ac2e332
Marek <marekbf3@gmail.com>
2019-05-15 22:35:49 +0200

    Ekran startowy - Dominik, Bartosz

---
+++ aplikacja/app/src/main/java/com/projekt/fuelprice/EkranStartowy.java
@@ -0,0 +1,13 @@
+package com.projekt.fuelprice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EkranStartowy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ekran_startowy);
+    }
+}
--- aplikacja/app/src/main/java/com/projekt/fuelprice/MainActivity.java
+++ aplikacja/app/src/main/java/com/projekt/fuelprice/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import com.projekt.fuelprice.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 3000;
 
     private ActivityMainBinding binding;
 
@@ -15,5 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent homeIntent = new Intent(MainActivity.this, EkranStartowy.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+
     }
 }
