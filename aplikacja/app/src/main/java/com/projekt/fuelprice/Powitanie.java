a369ab6cad4449ed322491d96b841134707394fe
Marek <marekbf3@gmail.com>
2019-05-15 22:55:02 +0200

    fix

--- aplikacja/app/src/main/java/com/projekt/fuelprice/Powitanie.java
+++ aplikacja/app/src/main/java/com/projekt/fuelprice/Powitanie.java
@@ -1,5 +1,7 @@
 package com.projekt.fuelprice;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,13 @@ public class Powitanie extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_powitanie);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), EkranStartowy.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
     }
 }
