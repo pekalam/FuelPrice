c7aaf84688a4353b401e44da65eb2d217ae2d035
Michal <m.rawski098@gmail.com>
2019-05-15 18:56:12 +0200

    .

--- aplikacja/app/src/main/java/com/projekt/fuelprice/GasStationsFragment.java
+++ aplikacja/app/src/main/java/com/projekt/fuelprice/GasStationsFragment.java
@@ -13,6 +13,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.support.v7.widget.RecyclerView;
 
 import com.projekt.fuelprice.data.GasStation;
 import com.projekt.fuelprice.databinding.FragmentGasStationsBinding;
