4df737a983eac7e161d69267c794e9d4a9b8932f
frogg1ven <bartekwikiera@gmail.com>
2019-05-21 23:44:54 +0200

    nieudolna proba ogarniecia paska XD

---
+++ aplikacja/app/src/main/java/com/projekt/fuelprice/AddPhotoBottomDialogFragment.java
@@ -0,0 +1,33 @@
+package com.projekt.fuelprice;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class AddPhotoBottomDialogFragment extends BottomSheetDialogFragment {
+
+    public static AddPhotoBottomDialogFragment newInstance() {
+        return new AddPhotoBottomDialogFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.activity_home, container,
+                false);
+
+
+        // get the views and attach the listener
+
+        return view;
+
+    }
+}
