0401d4caef8022769bea6824f66a0565a25d5f47
Marek <marekbf3@gmail.com>
2019-05-22 20:31:41 +0200

    bottom sheet 1 - Dominik, Bartosz

---
+++ aplikacja/app/src/main/java/com/projekt/fuelprice/FuelSelectionBottomSheetDialogFragment.java
@@ -0,0 +1,39 @@
+package com.projekt.fuelprice;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.projekt.fuelprice.databinding.FuelBottomSheetBinding;
+
+public class FuelSelectionBottomSheetDialogFragment extends BottomSheetDialogFragment {
+
+    private FuelBottomSheetBinding binding;
+
+    public static FuelSelectionBottomSheetDialogFragment newInstance() {
+        return new FuelSelectionBottomSheetDialogFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fuel_bottom_sheet, container, false);
+        binding.buttonReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+
+        return binding.getRoot();
+
+    }
+}
--- aplikacja/app/src/main/java/com/projekt/fuelprice/HomeActivity.java
+++ aplikacja/app/src/main/java/com/projekt/fuelprice/HomeActivity.java
@@ -1,19 +1,13 @@
 package com.projekt.fuelprice;
 
-import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 
 import com.crashlytics.android.Crashlytics;
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import com.projekt.fuelprice.databinding.ActivityHomeBinding;
-import com.projekt.fuelprice.services.PermissionsService;
-
 
 
 import javax.inject.Inject;
@@ -45,7 +39,17 @@ public class HomeActivity extends FragmentActivity implements HasSupportFragment
         //tak samo jest ze wszystkim czemu nadacie id w xml
         //dla kazdej innnej aktywnosci i fragmentu mozna robic tak samo
         binding = DataBindingUtil.setContentView(this, R.layout.activity_home);
-      
+        binding.bar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FuelSelectionBottomSheetDialogFragment fuelSelectionBottomSheetDialogFragment =
+                        FuelSelectionBottomSheetDialogFragment.newInstance();
+
+                fuelSelectionBottomSheetDialogFragment.show(getSupportFragmentManager(),
+                        "fuel_bottom_sheet");
+            }
+        });
+
     }
 
     @Override
